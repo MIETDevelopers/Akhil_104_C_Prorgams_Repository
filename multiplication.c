0d7214d345c279cbae24f6b991002dd5649c91a5
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-03-18 20:11:52 +0530

    multiplication of two numbers

--- multiplication.c
+++ multiplication.c
@@ -1,12 +1,12 @@
 //Program to Multiplication of two numbers
-#include<stdio.h>
+#include<stdio.h>//preprocessor directive to include standard input output function headerfile
 int main()
 {
-	int num1,num2,product;
-	printf("Enter two numbers:");
-	scanf("%d %d",&num1,&num2);
-	product=num1*num2;
-	printf("Product of two numbers: %d",product);
+	int num1,num2,product;// declaration of variables ie. num1,num2,product
+	printf("Enter two numbers:");//printf function to display the message on screen inside the double quotes
+	scanf("%d %d",&num1,&num2);//scanf function for taking the input values from user
+	product=num1*num2;// calculating the product of two numbers
+	printf("Product of two numbers: %d",product);//printf function to display the message on screen inside the double quotes
 	return  0;
 }
 				
