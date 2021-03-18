d618aa4134e8fe6d99def5d599c35d9ce4e6e9c2
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-03-18 20:16:26 +0530

    subtraction of two numbers

--- subtraction.c
+++ subtraction.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int num1,num2;
-    //variable declaration
+    int num1,num2; //variable declaration
     printf("Enter the first number: ");
-    scanf("%d",&num1);
-    //read the first number
+    scanf("%d",&num1);//read the first number
      printf("Enter the second number: ");
-    scanf("%d",&num2);
-    //read the second number
-    int sub=num2-num1;
-    //formula of subtraction
-    printf("Differences of two numbers are: %d\n",sub);
-    //display the result
-    getch();
+    scanf("%d",&num2);//read the second number
+    int sub=num2-num1;//formula of subtraction
+    printf("Differences of two numbers are: %d\n",sub);//display the result
     return 0;
 }
