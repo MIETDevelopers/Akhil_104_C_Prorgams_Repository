e2c977de9bf75328dc2804fc23314af9ebf40e61
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-03-18 20:20:29 +0530

    addition of two numbers

--- sum.c
+++ sum.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include<stdio.h>//preprocessor directive to include standard input output function headerfile
 int main() {
-int a, b, sum;
-printf("\nEnter two no: ");
-scanf("%d %d", &a, &b);
-sum = a + b;
-printf("Sum : %d", sum);
+int a, b, sum;//variable declaration
+printf("\nEnter two no: ");//printf function to display the message on screen inside the double quotes
+scanf("%d %d", &a, &b);//read the first number and second number
+sum = a + b;//formula of addition
+printf("Sum : %d", sum);//printf function to display the sum of two numbers
 return(0);
