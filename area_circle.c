0266db12169991209e02b08d55ba61dbb0224462
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-03-18 20:04:09 +0530

    find area of circle

--- area_circle.c
+++ area_circle.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter radius of circle\n");  //printf function for printing the message in double qoutes
     scanf("%f", & radius);  //scanf function for taking the input values from user
     area = PI * radius * radius;  //formula for calculating the area of circle
-    printf("Area of circle : %0.4f\n", area);  //printf function for printing the area
+    printf("Area of circle : %0.4f\n", area);  //printf function for printing the area of circle
 	return 0;
 }   
     
