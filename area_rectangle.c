524ee3015bfe3d63afeeefe3589256a0297dd825
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-03-19 09:56:18 +0530

    to calculate the perimetre of rectangle

--- area_rectangle.c
+++ area_rectangle.c
@@ -5,14 +5,14 @@ int main()
     float length,breadth;// declaration of variables ie. length and breadth
     float area;// declaration of variable ie. area
  
-    printf(" Enter the Length of a Rectangle : ");//printf function to dispay the message inside the double quotes
+    printf(" Enter the Length of a Rectangle : ");//printf function to dislpay the message inside the double quotes
     scanf("%f",&length);//scanf function for taking the input values from user
  
-    printf("\n Enter the Breadth of a Rectangle : ");//printf function to dispay the message inside the double quotes
+    printf("\n Enter the Breadth of a Rectangle : ");//printf function to dislpay the message inside the double quotes
     scanf("%f",&breadth);//scanf function for taking the input values from user
  
     area = length * breadth;//formula for calculating the area of the rectangle
  
-    printf("\n Area of Rectangle is : %f",area);//printf function to dispay the area of rectangle
+    printf("\n Area of Rectangle is : %f",area);//printf function to display the area of rectangle
     return 0;
 }
--- perimeter_rectangle.c
+++ perimeter_rectangle.c
@@ -5,14 +5,14 @@ int main()
     float length,breadth;// declaration of variables ie. length and breadth
     float perimeter;// declaration of variable ie. perimeter
  
-    printf(" Enter the Length of a Rectangle : ");//printf function to dispay the message inside the double quotes
+    printf(" Enter the Length of a Rectangle : ");//printf function to display the message inside the double quotes
     scanf("%f",&length);//scanf function for taking the input values from user
  
-    printf("\n Enter the Breadth of a Rectangle : ");//printf function to dispay the message inside the double quotes
+    printf("\n Enter the Breadth of a Rectangle : ");//printf function to dislplay the message inside the double quotes
     scanf("%f",&breadth);//scanf function for taking the input values from user
  
     perimeter = 2(length + breadth);//formula for calculating the perimeter of the rectangle
  
-    printf("\n perimeter of Rectangle is : %f",perimeter);//printf function to dispay the area of rectangle
+    printf("\n perimeter of Rectangle is : %f",perimeter);//printf function to display the area of rectangle
     return 0;
 }
