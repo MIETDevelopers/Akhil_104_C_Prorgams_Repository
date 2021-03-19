ef9049a231c342c59df1af9c73ea213054d8ea83
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-03-19 18:58:04 +0530

    Calculate the circumference of circle

--- circumference_circle.c
+++ circumference_circle.c
@@ -2,11 +2,11 @@
  #define PI 3.141  //define the value of PI
 int main()   
 {  
-    float radius, circumfrence;  // Declaration of variables ie. radius and circumfrence
+    float radius, circumference;  // Declaration of variables ie. radius and circumference
     printf("Enter radius of circle\n");  //printf function for printing the message in double qoutes
     scanf("%f", & radius);  //scanf function for taking the input values from user
-    circumfrence = 2* PI * radius ;  //formula for calculating the circumfrence of circle
-    printf("circumfrence of circle : %0.4f\n", circumfrence);  //printf function for printing the circumfrence of circle
+    circumference = 2* PI * radius ;  //formula for calculating the circumfrence of circle
+    printf("circumfrence of circle : %0.4f\n", circumference);  //printf function for printing the circumference of circle
 	return 0;
 }   
     
