8df1df17f27443ecc305ec732bbe284228661ace
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-03-22 15:25:32 +0530

    find the largest number

--- large_number.c
+++ large_number.c
@@ -1,25 +1,19 @@
-#include <stdio.h>
-#include <conio.h>
-
-//Largest Number - FScOnline.info
-
+#include <stdio.h>//preprocessor directive to include standard input output function headerfile  
 int main()
 {
-  int i,num,n,large=0;
+  int i,num,n,large=0;// integer variables declaration 
 
-  printf("How many numbers: ");
-  scanf("%d",&n);
+  printf("How many numbers: ");//Ask user to input how many numbers
+  scanf("%d",&n);//scanf function for taking the input values from user 
   
-  for(i=0; i<n; i++)
+  for(i=0; i<n; i++)// for loop from i=0 and i<n and incriment in i 
   {
     printf("\nEnter number %d: ",i+1);
-    scanf("%d",&num);
-    if(num>large)
-    large=num;
+    scanf("%d",&num);//scanf function for taking the input values from user 
+    if(num>large) // if statement for checking the num is greater then large
+    large=num; // the value of num is assigned to large
   }
 
-  printf("\n\nThe Largest Number is %d",large);
-  
-  getch();
+  printf("\n\nThe Largest Number is %d",large);//printf function for printing the largest number
   return 0;
 }
