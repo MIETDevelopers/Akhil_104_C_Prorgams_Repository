f1d903f7c3f9163ba73d3adb57df303b90abbe06
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-03-31 18:19:32 +0530

    hello world program

--- 3_Hello.c
+++ 3_Hello.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
+#include<stdio.h>//preprocessor directive to include standard input output function headerfile  
 int main()
 {
-	printf("Hello World");
+	printf("Hello World");  //printf function for printing the message  ie. inside double quotation marks
 	return 0;
 }
