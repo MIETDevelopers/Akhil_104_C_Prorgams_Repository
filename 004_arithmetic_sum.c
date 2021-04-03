329b2fac2c4cc8cf587237c4bf06ac9fc54a8985
Akhil Sumbria <2020a1r104@mietjammu.in>
2021-04-03 21:31:47 +0530

    program of sum of two numbers

---
+++ 004_arithmetic_sum.c
@@ -0,0 +1,8 @@
+#include<stdio.h>//preprocessor directive to include standard input output function headerfile
+int main() {
+int a, b, sum;// integer variable declaration
+printf("\nEnter two no: ");//printf function to display the message on screen inside the double quotes
+scanf("%d %d", &a, &b);//read the first number and second number
+sum = a + b;//formula of addition
+printf("Sum : %d", sum);//printf function to display the sum of two numbers
+return(0);
